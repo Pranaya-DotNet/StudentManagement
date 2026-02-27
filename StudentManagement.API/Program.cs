cbae6665a571eb3dacaf6168d47236004edffab9
Pranaya Rout <pranayakumar777@gmail.com>
2026-02-27 07:33:23 +0530

    Add CORS policy to allow all origins

--- StudentManagement.API/Program.cs
+++ StudentManagement.API/Program.cs
@@ -13,7 +13,20 @@ namespace StudentManagement.API
 
             builder.Services.AddSingleton<IStudentService, StudentService>();
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
             var app = builder.Build();
+
+            app.UseCors("AllowAll");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
