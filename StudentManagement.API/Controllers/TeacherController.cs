6724ef4c757bbe1c421cbd207c89231b6e7495ee
Pranaya Rout <pranayakumar777@gmail.com>
2026-02-27 06:57:25 +0530

    Added Teacher API Controller

---
+++ StudentManagement.API/Controllers/TeacherController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace StudentManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new[]
+            {
+                new { Id = 1, Name = "Teacher 1", Subject = "Math" },
+                new { Id = 2, Name = "Teacher 2", Subject = "Science" }
+            });
+        }
+    }
+}
